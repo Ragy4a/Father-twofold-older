87ac4145fcf08bc5e4a39ebed0a4e0a51eb5d951
Ragy4a <122722327+Ragy4a@users.noreply.github.com>
2024-02-08 13:15:48 +0200

    Production Version #1

--- src/index.js
+++ src/index.js
@@ -7,28 +7,32 @@
  */
 function calculateFatherAsTwiceOld(fathersAge, sonsAge) {
   let years = 0;
-  if (fathersAge === 0 && sonsAge === 0) {
-    return 'Неправильный ввод данных!'
+  if (fathersAge === 0) {
+    return 'Неправильный ввод данных!';
   }
   if (fathersAge < 0 || sonsAge < 0) {
     return `Возраст отца и сына должен быть положительным числом.`;
   }
-  if (fathersAge - sonsAge < 15 && sonsAge - fathersAge > 15) {
-    return 'Разница в возрасте отца и сына должна быть не меньше 15 лет.';
-  }
   if (fathersAge / 2 === sonsAge) {
     return `Отец уже вдвое старше сына.`;
-  } else if (fathersAge / 2 < sonsAge) {
-    years = fathersAge - (sonsAge * 2);
-    return years > 0 ? `Отец уже старше сына вдвое на ${years} лет.` : `Отец был старше сына вдвое ${-years} лет назад.`;
-  } else {
-    while (fathersAge / 2 > sonsAge) {
-      fathersAge++;
-      sonsAge++;
-      years++;
-    }
-    return `Отец будет старше сына вдвое через ${years} лет.`;
   }
+  if (Math.abs(fathersAge - sonsAge) < 15) {
+    return 'Разница в возрасте отца и сына должна быть не меньше 15 лет.';
+  }
+  if (fathersAge / 2 < sonsAge) {
+    years = fathersAge - sonsAge * 2;
+    return years > 0 ? `Отец уже старше сына вдвое на ${years} лет.` : `Отец был старше сына вдвое ${-years} лет назад.`;
+  }
+  if (fathersAge > sonsAge * 2) {
+    years = fathersAge - sonsAge * 2;
+    return years > 0 ? `Отец уже старше сына вдвое на ${years} лет.` : `Отец был старше сына вдвое ${-years} лет назад.`;
+  }
+  while (fathersAge / 2 > sonsAge) {
+    fathersAge++;
+    sonsAge++;
+    years++;
+  }
+  return `Отец будет старше сына вдвое через ${years} лет.`;
 }
 
 console.log(calculateFatherAsTwiceOld(0, 0));

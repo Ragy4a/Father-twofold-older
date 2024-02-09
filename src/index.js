0386ca91ea346f7999fc0dc74c403936c3aacd78
Ragy4a <122722327+Ragy4a@users.noreply.github.com>
2024-02-09 16:39:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@
  * @param {number} sonsAge - Возраст сына
  * @returns {number} - Информация о времени, через сколько лет или сколько лет назад отец будет/был вдвое старше сына
  */
-// debugger
 function calculateFatherAsTwiceOld(fathersAge, sonsAge) {
   let years = 0;
   if (fathersAge - sonsAge < 15 || sonsAge < 15) {
@@ -15,11 +14,14 @@ function calculateFatherAsTwiceOld(fathersAge, sonsAge) {
   if (fathersAge / 2 === sonsAge) {
     return `Отец уже вдвое старше сына.`;
   }
-  if (fathersAge < sonsAge * 2 || fathersAge > sonsAge * 2) {
+  if (fathersAge < sonsAge * 2) {
     years = fathersAge - (sonsAge * 2);
-    return years > 0  ? `Отец был старше сына вдвое ${years} лет/год/года назад.` :
-    `Отец будет старше сына вдвое ${-years} лет/год/года назад.`;
+    return `Отец был старше сына вдвое ${-years} лет/год/года.`
+  }
+  if (fathersAge > sonsAge * 2) {
+    years = fathersAge - (sonsAge * 2);
+    return `Отец был старше сына вдвое ${years} лет/год/года.`
   }
 }
 
-console.log(calculateFatherAsTwiceOld(50, 30));
+console.log(calculateFatherAsTwiceOld(61, 30));
